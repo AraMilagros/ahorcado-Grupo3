44a5de8dd73d41afb52e354db7270a0f878f029b
Lucas Martinez LLosco <linkgz55@gmail.com>
2022-10-06 18:31:23 -0300

    Se agrego el Css al juego -Lucas Martinez

--- src/components/Botones.js
+++ src/components/Botones.js
@@ -1,10 +1,11 @@
+import "../../src/assets/css/styleA.css";
 import React from "react";
 
 const Botones = (props) => {
     //Por cada elemento que recibe de abecedario.json retorna un botón.
     return (
-        <div>
-        <button disabled={props.deshabilitar} onClick={props.elegir} >{props.valor}</button>
+        <div className="contenedor">
+        <button className="botonesB" disabled={props.deshabilitar} onClick={props.elegir} >{props.valor}</button>
         </div>
       );
 }
